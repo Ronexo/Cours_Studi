e31c1313c94d4f4c48f501df12026408abf99b9a
Ron Exo <Ron83@live.fr>
2023-03-25 08:49:21 +0100

    Maj cours Js

--- 05_Develop_Front_End/02_Debut_Js/08_The_Condition/03_Switch_Condition/ex.js
+++ 05_Develop_Front_End/02_Debut_Js/08_The_Condition/03_Switch_Condition/ex.js
@@ -1,8 +1,30 @@
-// Setup
-let firstLetterOfLastName = "";
-const lastName = "Lovelace";
+let message;
+let tvStation = 18;
 
-// Only change code below this line
-firstLetterOfLastName = lastName[]; // Change this line
-
-console.log(lastName[]);
+switch (tvStation) {
+    case 1:
+        message = "Tf1";
+        break;
+    case 2:
+        message = "France 2"
+        break;
+    case 3:
+        message = "France 3";
+        break;
+    case 4:
+        message = "Canal +";
+        break;
+    case 5:
+         message = "France 5";
+         break;
+    case 6:
+        message = "M6";
+        break;
+    case 7:
+        message = "Arte";
+        break;
+    default:
+        message = "chaine inconue";
+    
+}
+console.log(message);
--- 05_Develop_Front_End/02_Debut_Js/08_The_Condition/04_Ex_Defi/ex.js
+++ 05_Develop_Front_End/02_Debut_Js/08_The_Condition/04_Ex_Defi/ex.js
@@ -1,30 +1,19 @@
-let message;
-let tvStation = 18;
+const el = document.getElementById('register-form');
+el.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-switch (tvStation) {
-    case 1:
-        message = "Tf1";
-        break;
-    case 2:
-        message = "France 2"
-        break;
-    case 3:
-        message = "France 3";
-        break;
-    case 4:
-        message = "Canal +";
-        break;
-    case 5:
-         message = "France 5";
-         break;
-    case 6:
-        message = "M6";
-        break;
-    case 7:
-        message = "Arte";
-        break;
-    default:
-        message = "chaine inconue";
-    
-}
-console.log(message);
+  let civility = event.target.female.checked ? 'female' : 'male';
+  let name = event.target.name.value;
+  let category = event.target.category.value;
+  let newsletter = event.target.newsletter.checked;
+
+  // Les variables déclarées ci-dessus contiennent les données du formulaire
+});
+
+civility ? 'femele' : 'male;
+
+alert(`Bonjour ${civility}  `)
+
+
+
+//"Bonjour [civilité de l'utilisateur] [nom de l'utilisateur]. ["Inscrivez-vous vite à notre newsletter" / "Merci de vous être abonné à notre newsletter"], elle permettra de vous envoyer de délicieuses recettes de votre catégorie préférée, [catégorie culinaire préférée de l'utilisateur]!"
